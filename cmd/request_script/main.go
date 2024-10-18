9c09d2fe0f29b7885e707e85087ca20ad0dc2412
bartosz_s <bartosz.starzec@websensa.com>
2024-10-18 14:31:50 +0200

    update request script, add makefile

--- cmd/request_script/main.go
+++ cmd/request_script/main.go
@@ -17,12 +17,13 @@ import (
 )
 
 var (
-	addr          = flag.String("addr", "localhost:8080", "http service address")
-	numChats      = flag.Int("chats", 5, "number of concurrent chats")
-	duration      = flag.Duration("duration", 1*time.Minute, "duration of the simulation")
-	totalSent     int64
-	totalReceived int64
-	startTime     time.Time
+	addr            = flag.String("addr", "localhost:8080", "http service address")
+	numChats        = flag.Int("chats", 5, "number of concurrent chats")
+	numConnsPerChat = flag.Int("conns", 3, "number of connections per chat")
+	duration        = flag.Duration("duration", 30*time.Second, "duration of the simulation")
+	totalSent       int64
+	totalReceived   int64
+	startTime       time.Time
 )
 
 type Message struct {
@@ -60,26 +61,41 @@ func main() {
 	close(done)
 	wg.Wait()
 
+	time.Sleep(5 * time.Second) // wait for any lagging msgs
 	printFinalStats(time.Since(startTime))
 }
 
 func runChat(id int, wg *sync.WaitGroup, done chan struct{}) {
 	defer wg.Done()
 
+	var connWg sync.WaitGroup
+	roomName := fmt.Sprintf("room_%d", id)
+
+	for j := 0; j < *numConnsPerChat; j++ {
+		connWg.Add(1)
+		go func(connID int) {
+			defer connWg.Done()
+			runConnection(id, connID, roomName, done)
+		}(j)
+	}
+
+	connWg.Wait()
+}
+
+func runConnection(chatID, connID int, roomName string, done chan struct{}) {
 	u := url.URL{Scheme: "ws", Host: *addr, Path: "/ws"}
-	log.Printf("Chat %d connecting to %s", id, u.String())
+	log.Printf("Chat %d, Conn %d connecting to %s", chatID, connID, u.String())
 
 	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
 	if err != nil {
-		log.Printf("Chat %d dial error: %v", id, err)
+		log.Printf("Chat %d, Conn %d dial error: %v", chatID, connID, err)
 		return
 	}
 	defer c.Close()
 
-	roomName := fmt.Sprintf("room_%d", id)
 	joinRoom(c, roomName)
 
-	go receiveMessages(id, c, done)
+	go receiveMessages(chatID, connID, c, done)
 
 	ticker := time.NewTicker(time.Duration(rand.Intn(5000)+1000) * time.Millisecond)
 	defer ticker.Stop()
@@ -89,13 +105,13 @@ func runChat(id int, wg *sync.WaitGroup, done chan struct{}) {
 		case <-done:
 			return
 		case <-ticker.C:
-			sendMessage(id, c, roomName)
+			sendMessage(chatID, connID, c, roomName)
 			ticker.Reset(time.Duration(rand.Intn(5000)+1000) * time.Millisecond)
 		}
 	}
 }
 
-func receiveMessages(id int, c *websocket.Conn, done chan struct{}) {
+func receiveMessages(chatID, connID int, c *websocket.Conn, done chan struct{}) {
 	for {
 		select {
 		case <-done:
@@ -103,26 +119,26 @@ func receiveMessages(id int, c *websocket.Conn, done chan struct{}) {
 		default:
 			_, message, err := c.ReadMessage()
 			if err != nil {
-				log.Printf("Chat %d read error: %v", id, err)
+				log.Printf("Chat %d, Conn %d read error: %v", chatID, connID, err)
 				return
 			}
 			atomic.AddInt64(&totalReceived, 1)
-			log.Printf("Chat %d received: %s", id, message)
+			log.Printf("Chat %d, Conn %d received: %s", chatID, connID, message)
 		}
 	}
 }
 
-func sendMessage(id int, c *websocket.Conn, roomName string) {
+func sendMessage(chatID, connID int, c *websocket.Conn, roomName string) {
 	message := Message{
 		Type:      "chat",
-		Content:   fmt.Sprintf("Message from chat %d at %v", id, time.Now()),
-		Sender:    fmt.Sprintf("Client_%d", id),
+		Content:   fmt.Sprintf("Message from chat %d, conn %d at %v", chatID, connID, time.Now()),
+		Sender:    fmt.Sprintf("Client_%d_%d", chatID, connID),
 		Timestamp: time.Now(),
 		Room:      roomName,
 	}
 	err := c.WriteJSON(message)
 	if err != nil {
-		log.Printf("Chat %d write error: %v", id, err)
+		log.Printf("Chat %d, Conn %d write error: %v", chatID, connID, err)
 		return
 	}
 	atomic.AddInt64(&totalSent, 1)
@@ -146,6 +162,8 @@ func printFinalStats(duration time.Duration) {
 	fmt.Printf("\n--- Final Statistics ---\n")
 	fmt.Printf("Duration: %v\n", duration)
 	fmt.Printf("Total Chats: %d\n", *numChats)
+	fmt.Printf("Connections per Chat: %d\n", *numConnsPerChat)
+	fmt.Printf("Total Connections: %d\n", *numChats**numConnsPerChat)
 	fmt.Printf("Messages Sent: %d\n", sent)
 	fmt.Printf("Messages Received: %d\n", received)
 	fmt.Printf("Send Rate: %.2f messages/second\n", float64(sent)/duration.Seconds())
